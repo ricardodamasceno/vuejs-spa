0dbf62763527ca3f58ada5c966364e330034889e
Ricardo Damasceno <ricardodamascenno@gmail.com>
2017-11-24 00:07:59 -0200

    Addition of form to test the application

--- src/app/views/index.js
+++ src/app/views/index.js
@@ -1,6 +1,27 @@
-var app = new Vue({
-    el: '#app',
+let app = new Vue({
+    el: '#index-form',
     data: {
-        message: 'Hello Vue!'
+        usersCount : 1,
+        userId: undefined,
+        userName:  '',
+        userEmail: '',
+        users: []
+    },
+    methods: {
+        addUser: function () {
+            let user = {id: this.usersCount, name : this.userName, email: this.userEmail};
+            this.users.push(user);
+            this.userName = '';
+            this.userEmail = '';
+            this.userId = undefined;
+            this.usersCount++;
+        },
+        removeUser:function(userId){
+            for(let i=0;i<this.users.length;i++){
+                if(this.users[i].id == userId){
+                    this.users.splice(i,1);
+                }
+            }
+        }
     }
 });
